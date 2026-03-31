136297c7861f7317850eef8991e56a8c69f30e41
DooPrjkt <gitorrb@gmail.com>
2026-03-31 10:51:16 +0700

    Add build automation script for Axion AOSP
    This script automates the process of initializing a repo, syncing sources, cleaning old outputs, and building GMS and Vanilla versions of the project.

---
+++ axion-v2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+rm -rf .repo/local_manifests
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+
+# Clone local manifest
+git clone https://github.com/DooPrjkt/local_manifest -b main .repo/local_manifests
+
+echo "==> Syncing sources..."
+if [ -f /opt/crave/resync.sh ]; then
+    /opt/crave/resync.sh
+else
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+
+echo "==> Cleaning old outputs..."
+dirs_to_remove=(
+    out/target/product/munch
+    out/target/product/gapps
+    out/target/product/vanilla
+)
+rm -rf "${dirs_to_remove[@]}"
+
+echo "=== Starting GMS (Pico) build ==="
+. build/envsetup.sh
+axion X1 user gms pico
+ax -br
+mv out/target/product/munch out/target/product/gapps
+
+echo "=== Starting Vanilla (AOSP) build ==="
+. build/envsetup.sh
+axion munch user va
+ax -br
+mv out/target/product/munch out/target/product/vanilla
+
+echo "=== All builds completed successfully! ==="
