678b8152c0fe9a58165ba4f6d2cab5fb10713368
DooPrjkt <gitorrb@gmail.com>
2026-04-07 15:57:54 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -3,11 +3,12 @@ rm -rf prebuilts/clang/host/linux-x86
 rm -rf device/xiaomi/mido
 rm -rf kernel/xiaomi/mido
 rm -rf vendor/xiaomi/mido
+rm -rf .repo/local_manifests
 
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectMatrixx/android -b 16.2 -g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/DooPrjkt/local_manifest/blob/matrixx-bka/local_manifest.xml -b matrixx-bka .repo/local_manifests
-/opt/crave/resync.sh || repo sync
+git clone https://github.com/DooPrjkt/local_manifest.git -b matrixx-bka .repo/local_manifests
+/opt/crave/resync.sh || repo sync --force-sync --no-clone-bundle --no-tags
 
 export BUILD_USERNAME=mibombo
 export BUILD_HOSTNAME=crave
