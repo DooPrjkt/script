65d40b11335b887b726f2c2b2b6f2de016bf2e19
DooPrjkt <gitorrb@gmail.com>
2026-04-12 20:23:55 +0700

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf frameworks/base
-rm -rf packages/apps/Settings
-rm -rf build/soong
-rm -rf vendor/custom
 rm -rf .repo/local_manifests
 
 repo init --no-repo-verify --git-lfs -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/mibomboq/local_manifest.git -b main .repo/local_manifests
+git clone https://github.com/DooPrjkt/local_manifest.git -b pixelOS .repo/local_manifests
 /opt/crave/resync.sh || repo sync --force-sync --no-clone-bundle --no-tags
 
 export BUILD_USERNAME=mibombo
