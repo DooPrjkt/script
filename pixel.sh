0e2822f3db78be00ad6e5eaa870cd679aefa3293
DooPrjkt <gitorrb@gmail.com>
2026-04-25 00:11:08 +0700

    Add clone command for android_vendor_sony_dolby

--- pixel.sh
+++ pixel.sh
@@ -15,7 +15,7 @@ git clone --depth=1 https://github.com/G100-X1/android_device_advan_X1-kernel.gi
 git clone https://github.com/G100-X1/device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
 git clone https://github.com/G100-X1/hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/G100-X1/android_vendor_mediatek_ims.git -b lineage-23.1 vendor/mediatek/ims
-
+git clone https://github.com/G100-X1/android_vendor_sony_dolby.git -b sixteen-redesign vendor/sony/dolby
 git clone https://github.com/putraaxzy/packages_apps_GameBar packages/apps/GameBar
 
 export BUILD_USERNAME=miku
