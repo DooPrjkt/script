01a5eb12e47d35890361656d167e025a06dea48b
DooPrjkt <gitorrb@gmail.com>
2026-06-12 16:40:14 +0700

    Update kernel repository for Advan X1

--- infinity.sh
+++ infinity.sh
@@ -6,13 +6,12 @@ repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/man
 
 git clone --depth=1 https://github.com/DooPrjkt/android_device_advan_X1.git -b infinity-wip device/advan/X1
 git clone --depth=1 https://github.com/G100-X1/android_vendor_advan_X1.git -b lineage-23.2 vendor/advan/X1
-git clone --depth=1 https://github.com/G100-X1/android_device_advan_X1-kernel.git -b lineage-23.1 device/advan/X1-kernel
+git clone --depth=1 https://github.com/orrb7/android_device_advan_X1-kernel.git -b master device/advan/X1-kernel
 
 git clone https://github.com/G100-X1/device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
 git clone https://github.com/G100-X1/hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/G100-X1/android_vendor_mediatek_ims.git -b lineage-23.1 vendor/mediatek/ims
 git clone https://github.com/G100-X1/android_vendor_sony_dolby.git -b sixteen-redesign vendor/sony/dolby
-git clone https://github.com/putraaxzy/packages_apps_GameBar packages/apps/GameBar
 
 export BUILD_USERNAME=miku
 export BUILD_HOSTNAME=crave
