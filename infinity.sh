7a47be3e4d9d78ed4ff17cdca795057d86f643ea
DooPrjkt <gitorrb@gmail.com>
2026-06-12 17:40:40 +0700

    Fix git clone command for kernelDummy repository

--- infinity.sh
+++ infinity.sh
@@ -8,7 +8,7 @@ git clone --depth=1 https://github.com/DooPrjkt/android_device_advan_X1.git -b i
 git clone --depth=1 https://github.com/G100-X1/android_vendor_advan_X1.git -b lineage-23.2 vendor/advan/X1
 git clone --depth=1 https://github.com/orrb7/android_device_advan_X1-kernel.git -b master device/advan/X1-kernel
 
-https://github.com/DooPrjkt/kernelDummy.git -b main kernel/advan/X1
+git clone https://github.com/DooPrjkt/kernelDummy.git -b main kernel/advan/X1
 
 git clone https://github.com/G100-X1/device_mediatek_sepolicy_vndr.git -b lineage-23.1 device/mediatek/sepolicy_vndr
 git clone https://github.com/G100-X1/hardware_mediatek.git -b lineage-23.1 hardware/mediatek
